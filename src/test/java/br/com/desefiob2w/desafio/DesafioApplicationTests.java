ae67cfd85c1106d494da3052c8b7a85eaf61bd34
GuiSampaio <gui85238523@gmail.com>
2020-07-06 23:21:58 -0300

    Adicionando alguns testes,novo banner.

--- src/test/java/br/com/desefiob2w/desafio/DesafioApplicationTests.java
+++ src/test/java/br/com/desefiob2w/desafio/DesafioApplicationTests.java
@@ -1,13 +1,15 @@
 package br.com.desefiob2w.desafio;
 
-import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+
+
+
 @SpringBootTest
+@AutoConfigureMockMvc
 class DesafioApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
